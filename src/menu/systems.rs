bfc38492371b81a6e55117ebe4f5c943a7175e4c
Liam Pugh <liam.pugh.009@gmail.com>
2023-03-25 18:57:39 +0000

    ui started

--- src/menu/systems.rs
+++ src/menu/systems.rs
@@ -1,5 +1,27 @@
 use bevy::prelude::*;
 
-pub fn say_hi() {
-    println!("Hello");
+pub fn say_hi(
+    mut commands: Commands,
+) {
+    commands.spawn(NodeBundle {
+        style: Style {
+            size: Size {
+                width: Val::Px(150.),
+                height: Val::Px(25.)
+            },
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        ..default()
+    }).with_children(| parent | {
+        parent.spawn(NodeBundle {
+            style: Style {
+                size: Size::width(Val::Percent(50.)),
+                border: UiRect::all(Val::Percent(100.))
+                ,..default()
+            },
+            background_color: Color::ALICE_BLUE.into(),
+            ..default()
+        });
+    });
 }
